571c15ea67ba362d81d4b6c3c8505b2e44f923d3
bruno3du <bruno.3duardo@hotmail.com>
2021-11-03 17:08:56 -0300

    "Initial commit"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+import { Header } from './Header';
+import { GlobalStyled } from './styles/global';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+	return (
+		<>
+			<GlobalStyled />
+			<Header />
+			<h1>Hello World</h1>
+		</>
+	);
 }
 
 export default App;
---
+++ src/Header/index.tsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import dtMoneyLogo from '../assets/logo.svg';
+import { Container, Content } from './styles';
+
+export function Header() {
+	return (
+		<Container>
+			<Content>
+				<img src={dtMoneyLogo} alt='dt money' />
+				<button type='button'>Nova transação</button>
+			</Content>
+		</Container>
+	);
+}
